70183b7bc2433065ec548065c3e935c1a251a3cb
DragonH <a0261038@g2.usc.edu.tw>
2019-09-20 20:55:26 +0800

    fix check new post issue

--- index.js
+++ index.js
@@ -11,19 +11,22 @@
 (function() {
   'use strict';
   try {
+    if (!$('[name="subject"]').length) {
+      return false;
+    }
     const editor = $('#SimpleMDE_0').data('simplemde');
     editor.options.autosave = {
-        enabled: true,
-        uniqueId: "He-Says-I-Need-A-uniqueId",
-        delay: 1000,
+      enabled: true,
+      uniqueId: "He-Says-I-Need-A-uniqueId",
+      delay: 1000,
     };
     editor.autosave();
     if ($('.save-group__btn').length) {
-        $('.save-group__btn').click((e) => {
-          if (!confirm('確定使用儲存草稿? (可能會與此auto save plugin 衝突)')) {
-              return false;
-          }
-        });
+      $('.save-group__btn').click((e) => {
+        if (!confirm('確定使用儲存草稿? (可能會與此auto save plugin 衝突)')) {
+            return false;
+        }
+      });
     }
     alert('Auto save 載入成功!');
   } catch (err) {
